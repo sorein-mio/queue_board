09ef136c96a930d57d74c0dea01070814618800d
miosugiyama <m-sugiyama@tokyoweld.com>
2023-01-31 16:56:18 +0900

    Chap9 掲示板作成機能

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,14 +1,23 @@
 class BoardsController < ApplicationController
   def new
+    @board = Board.new
   end
 
   def show
+    @board = Board.find(params[:id])
   end
 
   def edit
   end
 
   def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.aleat')
+      render :new
+    end
   end
 
   def destroy
@@ -16,5 +25,12 @@ class BoardsController < ApplicationController
 
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
+  end
+
+  private
+
+
+  def board_params
+    params.require(:board).permit(:title, :body)
   end
 end
