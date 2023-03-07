e748ee941da97b9e1f0b4f0e87b0b37ba15dee16
miosugiyama <m-sugiyama@tokyoweld.com>
2023-03-07 16:31:53 +0900

    Chap17 コメント投稿・編集・削除のAjax

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :boards do
-    resources :comments, only: %i(create update destroy), shallow: true
+    resources :comments, shallow: true
     collection do
       get :bookmarks
     end
