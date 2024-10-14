264eefa2fc08efbf920096456c72730a4f05046c
sorein30-at-033006399970 <sorein30-at-033006399970>
2024-10-14 20:09:57 +0900

    サービス名変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def page_title(page_title = '')
-        base_title = 'RUNTEQ BOARD APP'
+        base_title = 'SOREIN BOARD APP'
         page_title.empty? ? base_title : page_title + " | " + base_title
     end
 end
